4e6194e6c5becabe59561997632559ebbe3e88ae
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-06-13 10:47:56 +0530

    added cancel listing

--- contracts/NFTMarketPlace.sol
+++ contracts/NFTMarketPlace.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Provider: MIT
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
 
 // Imports
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
@@ -46,6 +46,94 @@ contract NFTMarketPlace is ReentrancyGuard {
         uint256 indexed tokenId,
         uint256 price
     );
+
+    // Mappings
+    // NFT Contract Address -> NFT Token ID -> Listing
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    // Seller Address -> Amount Earned
+    mapping(address => uint256) private s_proceeds;
+
+    ////////////////
+    // Modifiers //
+    //////////////
+
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert NFTMarketPlace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price <= 0) {
+            revert NFTMarketPlace__NotListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address seller
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (seller != owner) {
+            revert NFTMarketPlace__NotOwner();
+        }
+        _;
+    }
+
+    /////////////////////
+    // Main Functions //
+    ///////////////////
+
+    /*
+     * @notice Method for listing NFT
+     * @param nftAddress Address of NFT contract
+     * @param tokenId Token ID of NFT
+     * @param price sale price for each item
+     */
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+        external
+        notListed(nftAddress, tokenId, msg.sender)
+        isOwner(nftAddress, tokenId, msg.sender)
+    {
+        if (price <= 0) {
+            revert NFTMarketPlace__PriceMustBeAboveZero();
+        }
+        IERC721 nft = IERC721(nftAddress);
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert NFTMarketPlace__NotApprovedForMarketplace();
+        }
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    /*
+     * @notice Method for cancelling listing
+     * @param nftAddress Address of NFT contract
+     * @param tokenId Token ID of NFT
+     */
+    function cancelListing(address nftAddress, uint256 tokenId)
+        external
+        isOwner(nftAddress, tokenId, msg.sender)
+        isListed(nftAddress, tokenId)
+    {
+        delete (s_listings[nftAddress][tokenId]);
+        emit ItemCanceled(msg.sender, nftAddress, tokenId);
+    }
 
     ///////////////////////
     // Getter Functions //
